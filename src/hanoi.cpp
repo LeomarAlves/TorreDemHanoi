66d53be850d8f6b156feefc241c00bd9a89ad07a
Leomar Alves Marcelino <leo.dev.jobs@gmail.com>
2026-04-23 23:42:56 -0300

    feat(hanoi): cria função resolverHanoi()

--- src/hanoi.cpp
+++ src/hanoi.cpp
@@ -11,3 +11,20 @@ void inicializarHastes(int n, stack<int>& a, stack<int>& b, stack<int>& c){
     
 }
 
+void resolverHanoi(int n, stack<int>& origem, stack<int>& destino, stack<int>& auxiliar, char nomeOrigem, char nomeDestino, char nomeAuxiliar) {
+    
+    if (n == 1) {
+        int disco = origem.top();
+        destino.push(disco);
+        origem.pop();
+        cout << "Mover disco " << disco << " de " << nomeOrigem << " para " << nomeDestino << endl;
+        return;
+    }
+
+    resolverHanoi(n - 1, origem, auxiliar, destino, nomeOrigem, nomeAuxiliar, nomeDestino);
+
+    resolverHanoi(1, origem, destino, auxiliar, nomeOrigem, nomeDestino, nomeAuxiliar);
+
+    resolverHanoi(n - 1, auxiliar, destino, origem, nomeAuxiliar, nomeDestino, nomeOrigem);
+
+}
--- testes/test_hanoi.cpp
+++ testes/test_hanoi.cpp
@@ -9,11 +9,11 @@ void resolverHanoi(int n, stack<int>& origem, stack<int>& destino, stack<int>& a
 
 int main(){
     stack<int> hasteA, hasteB, hasteC;
-    int n = 3;
+    int n = 5;
 
     inicializarHastes(n, hasteA, hasteB, hasteC);
 
-    assert(hasteA.size() == 3);
+    assert(hasteA.size() == 5);
     assert(hasteB.empty() == true);
     assert(hasteC.empty() == true);
     assert(hasteA.top() == 1);
@@ -24,7 +24,7 @@ int main(){
 
     assert(hasteA.empty() == true);
     assert(hasteB.empty() == true);
-    assert(hasteC.size() ==3);
+    assert(hasteC.size() ==5);
     assert(hasteC.top() == 1);
 
     cout << "Teste de resolução da Torre de Hanoi passou com sucesso!" << endl;
